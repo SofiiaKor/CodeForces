d0f47a9fd74ae8a6abbd689b75dda361afbad2cb
SofiiaKor <SofiiaKor@github.com>
2022-02-06 17:13:53 +0200

    1248 - Added Task3

---
+++ CodeForces/Tasks/Task3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeForces.Tasks
+{
+    class Task3
+    {
+        public void Execute()
+        {
+        }
+    }
+}
